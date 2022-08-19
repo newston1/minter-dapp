7d34bffde4f784802a62418470908530fabd2707
newston1 <piratepartysociety@gmail.com>
2022-08-19 21:35:40 +0200

    bvbvbvbv

--- backend/src/config.js
+++ backend/src/config.js
@@ -59,7 +59,7 @@ const TOKENS_PER_MINT = 250; // maximum number of NFTs a user can mint in a sing
 const PUBLIC_MINT_START_DATE = "2022-11-19T13:56:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = null; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = "2022-07-19T13:56:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0xF2AD0B59cDBC2602c9eDA62B5F388F362534D84a"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
