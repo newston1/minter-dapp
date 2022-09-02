94521aea3bdd3bfd82f59d9b78ea7957f72a3598
newston1 <piratepartysociety@gmail.com>
2022-09-02 17:50:13 +0200

    q

--- backend/src/config.js
+++ backend/src/config.js
@@ -56,7 +56,7 @@ const MINT_PRICE = 0.0001; // Minting price per NFT. Rinkeby = ETH, Ethereum = E
 const TOKENS_PER_MINT = 250; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PUBLIC_MINT_START_DATE = "2022-09-2T15:50:35+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
+const PUBLIC_MINT_START_DATE = "2022-09-02T16:00:35+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
 const PRESALE_MINT_START_DATE = "null"; // Optional. Eg: 2022-02-08T11:30:48+00:00
